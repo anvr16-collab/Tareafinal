e7b732385dbe85035040585cdb46ab4210eb8508
anvr16-collab <anvr16@gmail.com>
2025-11-29 13:56:03 -0400

    Delete Trabajofinalapp directory

--- Trabajofinalapp/Empleado.cs
+++
@@ -1,8 +0,0 @@
-public class Empleado
-{
-    public int Id { get; set; }
-    public string Codigo { get; set; }
-    public string Nombre { get; set; }
-    public string Departamento { get; set; }
-    public decimal SalarioBase { get; set; }
-}
